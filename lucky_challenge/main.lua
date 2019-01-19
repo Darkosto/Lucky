941644bd56489473e3e2e87ff8e9a6bd5c8f59e3
Darkosto <darkostotv@gmail.com>
2019-01-19 14:55:00 -0800

    Apparently symlinks do not work

--- lucky_challenge/main.lua
+++ lucky_challenge/main.lua
@@ -1,1 +1,21 @@
-C:/Users/darkosto/Documents/My Games/Binding of Isaac Afterbirth+ Mods/lucky_challenge/main.lua
+local mod = RegisterMod("Lucky!1!", 1)
+local game = Game()
+	
+
+local Challenges = {
+	CHALLENGE_LUCKY = Isaac.GetChallengeIdByName("Lucky!1!")
+}
+
+
+function mod:onPlayerInit(player)
+	if game.Challenge == Challenges.CHALLENGE_LUCKY then 
+
+
+	    --Add 10 Luck
+		for i = 1, 10, 1	do
+		    player:UsePill(PillEffect.PILLEFFECT_LUCK_UP, PillColor.PILL_NULL)
+		end
+	end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.onPlayerInit)
