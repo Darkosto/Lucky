1384466a395cdee0510e65ad8f9a8f787b1e1257
Darkosto <darkostotv@gmail.com>
2019-01-19 16:48:58 -0800

    Added Luck Down effect on coin pickup
    Still need to figure out how to have stat change immediately

--- lucky_challenge/main.lua
+++ lucky_challenge/main.lua
@@ -1,15 +1,14 @@
 local mod = RegisterMod("Lucky!1!", 1)
 local game = Game()
-	
 
 local Challenges = {
 	CHALLENGE_LUCKY = Isaac.GetChallengeIdByName("Lucky!1!")
 }
 
-
+--Set starting coins to 0 and add 10 luck
 function mod:onPlayerInit(player)
 	if game.Challenge == Challenges.CHALLENGE_LUCKY then 
-
+	player:AddCoins(-99)
 
 	    --Add 10 Luck
 		for i = 1, 10, 1	do
@@ -18,4 +17,30 @@ function mod:onPlayerInit(player)
 	end
 end
 
+--If player picks up a coin then luck down is applied
+local currCoins = 0;
+local isStarted = false;
+ 
+function mod:setStartingConsumables()
+	if game.Challenge == Challenges.CHALLENGE_LUCKY then 
+		if(isStarted == false) then
+			currCoins = player:GetNumCoins();
+			isStarted = true;
+		end
+	end
+end
+
+function mod:checkConsumables()
+    player = Isaac.GetPlayer(0);
+	if game.Challenge == Challenges.CHALLENGE_LUCKY then 
+   
+		if(currCoins < player:GetNumCoins()) then
+			player:UsePill(PillEffect.PILLEFFECT_LUCK_DOWN, PillColor.PILL_NULL)
+		end
+    currCoins = player:GetNumCoins();
+end
+end
+ 
+mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.setStartingConsumables);
+mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.checkConsumables);
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.onPlayerInit)
